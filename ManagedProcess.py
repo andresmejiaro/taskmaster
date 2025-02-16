a021d9134074e59598cd4c9609bfc4199fe6c063
andresM <andresmejiaro@gmail.com>
2025-02-16 13:44:16 -0500

    some stopping things

--- ManagedProcess.py
+++ ManagedProcess.py
@@ -1,4 +1,4 @@
-import subprocess, shlex
+import subprocess, shlex, signal
 from enum import Enum
 from datetime import datetime
 
@@ -9,10 +9,13 @@ class ProcessStatus(Enum):
     RUNNING = "running"
     STARTiNG = "starting"
     CRASHED = "crashed"
+    STOPING = "stoping"
 
 
-
-
+class ReestartOptions(Enum):
+    ALWAYS = "always"
+    NEVER = "never"
+    CRASH = "crash"
 
 class ManagedProcess:
     def __init__(self, jsonAtt):
@@ -23,14 +26,22 @@ class ManagedProcess:
         self.autostart = jsonAtt.get("autostart",False)
         self.exitcodes = jsonAtt.get("exitcodes",[0])
         self.starttime = jsonAtt.get("starttime",0)
-        self.stopsignal = jsonAtt.get("stopsignal","TERM")
+        signal_name = jsonAtt.get("stopsignal","TERM")  # Or any other signal name without the 'SIG' prefix.
+        try:
+            sig = getattr(signal, "SIG" + signal_name)
+        except AttributeError:
+            print(f"Invalid signal name: SIG{signal_name}, using SIGTERM instead to finish")
+            sig = getattr(signal, "SIGTERM")
+        self.stopsignal = sig
         self.stoptime = jsonAtt.get("stoptime",10)
         self.stdout = jsonAtt.get("stdout","/dev/null")
         self.stderr = jsonAtt.get("stderr","/dev/null")
         self.env = jsonAtt.get("env",{})
         self.status = ProcessStatus.STOPPED
         self.initTime = None
+        self.breakTime = None
         self.process = None
+        self.restarting = 0
        
 
     def updateStatus(self):
@@ -38,6 +49,11 @@ class ManagedProcess:
         if self.process is None:
             return
         pollresult = self.process.poll()
+        if self.restarting == True and \
+            self.status == ProcessStatus.RUNNING:
+            self.restarting = False
+        if self.restarting == True:
+            self.restartProcess()
         match self.status:
             case ProcessStatus.STOPPED:
                 pass
@@ -50,6 +66,8 @@ class ManagedProcess:
                     self.status = ProcessStatus.STOPPED
                 elif pollresult not in self.exitcodes:
                     self.status = ProcessStatus.CRASHED
+            case ProcessStatus.STOPING:
+                self.stopProcess()
 
 
     def launchProcess(self):
@@ -73,12 +91,28 @@ class ManagedProcess:
  
         
 
-    def killProcess(self):
-        pass
+    def stopProcess(self):
+
+        if self.breakTime is not None:
+            difftime = datetime.now() - self.breakTime()
+        else:
+            difftime = None
+        if self.status == ProcessStatus.STOPING and difftime is not None :
+            if difftime > self.stoptime:
+                self.process.kill()
+                self.status = ProcessStatus.STOPPED           
+        if self.status == ProcessStatus.RUNNING:
+            self.status = ProcessStatus.STOPING
+            self.process.send_signal(self.stopsignal)
+            self.breakTime = datetime.now()
+
 
     def restartProcess(self):
-        self.killProcess()
-        self.launchProcess()
+        self.restarting = True
+        if self.status == ProcessStatus.RUNNING:
+            self.stopProcess()
+        if self.status in (ProcessStatus.STOPPED, ProcessStatus.CRASHED)
+            self.launchProcess()
 
     def outputGet(self):
         pass
